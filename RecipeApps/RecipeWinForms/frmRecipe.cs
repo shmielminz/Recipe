860f3b0519ca9db8784aa8bfd125f50635038b19
Shmiel Minz <s0583223395@gmail.com>
2023-02-02 00:04:36 +0000

    Updated recipe

--- RecipeApps/RecipeWinForms/frmRecipe.cs
+++ RecipeApps/RecipeWinForms/frmRecipe.cs
@@ -8,29 +8,55 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using CPUFramework;
+using CPUWindowsFormFramework;
 
 namespace RecipeWinForms
 {
     public partial class frmRecipe : Form
     {
+        DataTable dtrecipe = new();
         public frmRecipe()
         {
             InitializeComponent();
+            btnSave.Click += BtnSave_Click;
+            btnDelete.Click += BtnDelete_Click;
         }
 
         public void ShowForm(int recipeid)
         {
             string sql = "select r.*, c.CuisineType, s.Username from Recipe r join Cuisine c on c.CuisineId = r.CuisineId join Staff s on s.StaffId = r.StaffId where r.RecipeId = " + recipeid.ToString();
-            DataTable dt = SQLUtility.GetDataTable(sql);
-            txtRecipeName.DataBindings.Add("Text", dt, "RecipeName");
-            lblStaffName.DataBindings.Add("Text", dt, "Username");
-            lblCuisine.DataBindings.Add("Text", dt, "CuisineType");
-            txtCalories.DataBindings.Add("Text", dt, "Calories");
-            txtDateDrafted.DataBindings.Add("Text", dt, "DateDrafted");
-            txtDatePublished.DataBindings.Add("Text", dt, "DatePublished");
-            txtDateArchived.DataBindings.Add("Text", dt, "DateArchived");
-            lblRecipeStatus.DataBindings.Add("Text", dt, "RecipeStatus");
+            dtrecipe = SQLUtility.GetDataTable(sql);
+            DataTable dtstaff = SQLUtility.GetDataTable("select StaffId, Username from Staff");
+            DataTable dtcuisine = SQLUtility.GetDataTable("select CuisineId, CuisineType from Cuisine");
+            WindowsFormsUtility.SetControlBinding(txtRecipeName, dtrecipe);
+            WindowsFormsUtility.SetListBinding(lstUsername, dtstaff, dtrecipe, "Staff");
+            WindowsFormsUtility.SetListBinding(lstCuisineType, dtcuisine, dtrecipe, "Cuisine");
+            WindowsFormsUtility.SetControlBinding(txtCalories, dtrecipe);
+            WindowsFormsUtility.SetControlBinding(txtDateDrafted, dtrecipe);
+            WindowsFormsUtility.SetControlBinding(txtDatePublished, dtrecipe);
+            WindowsFormsUtility.SetControlBinding(txtDateArchived, dtrecipe);
+            WindowsFormsUtility.SetControlBinding(lblRecipeStatus, dtrecipe);
             this.Show();
+        }
+
+        private void Save()
+        {
+
+        }
+
+        private void Delete()
+        {
+
+        }
+
+        private void BtnDelete_Click(object? sender, EventArgs e)
+        {
+            Delete();
+        }
+
+        private void BtnSave_Click(object? sender, EventArgs e)
+        {
+            Save();
         }
     }
 }
