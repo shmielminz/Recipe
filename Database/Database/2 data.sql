62e42241905b415a4b6dba61f97be6380cf7afec
Shmiel Minz <s0583223395@gmail.com>
2023-03-13 20:10:10 +0000

    Updated table and data

--- Database/Database/2 data.sql
+++ Database/Database/2 data.sql
@@ -47,12 +47,12 @@ go
 ;with x as(
    select Username = 'johnsmith@heartyhearth.com', RecipeName = 'Chocolate Chip Cookies', Cuisine = 'American', Calories = 148, Drafted = dateadd(month,-2,getdate()), Published = getdate(), Archived = null
    union select 'mike@heartyhearth.com', 'Apple Yogurt Smoothie', 'French', 60, getdate(), null, null
-   union select 'mike@heartyhearth.com', 'Cheese Bread', 'English', 71, dateadd(day,-1,getdate()), null, getdate()
-   union select 'robert@heartyhearth.com', 'Butter Muffins', 'American', 170, dateadd(day,-10,getdate()), dateadd(day,-10,getdate()), getdate()
-   union select 'johnroberts@heartyhearth.com', '360-Minute Steak', 'American', 210, getdate(), null, null
-   union select 'mike@heartyhearth.com', 'Roasted Vegetable Yapchik', 'English', 60, dateadd(day,-1,getdate()), null, getdate()
+   union select 'mike@heartyhearth.com', 'Cheese Bread', 'English', 71, dateadd(month,-2,getdate()), null, getdate()
+   union select 'robert@heartyhearth.com', 'Butter Muffins', 'American', 170, dateadd(day,-10,getdate()), dateadd(day,-1,getdate()), getdate()
+   union select 'johnroberts@heartyhearth.com', '360-Minute Steak', 'American', 210, dateadd(day,-10,getdate()), null, getdate()
+   union select 'mike@heartyhearth.com', 'Roasted Vegetable Yapchik', 'English', 60, dateadd(day,-2,getdate()), null, null
    union select 'johnroberts@heartyhearth.com', 'Yemenite Chicken Soup', 'Middle East', 150, dateadd(day,-25,getdate()), getdate(), null
-   union select 'johnsmith@heartyhearth.com', 'Eggplant Casserole Dip', 'French', 75, getdate(), getdate(), null
+   union select 'johnsmith@heartyhearth.com', 'Eggplant Casserole Dip', 'French', 75, dateadd(month,-2,getdate()), null, getdate()
 )
 insert Recipe(StaffId,CuisineId,RecipeName,Calories,DateDrafted,DatePublished,DateArchived)
 select s.StaffId, c.CuisineId, x.RecipeName, x.Calories, x.Drafted, x.Published, x.Archived
@@ -388,7 +388,7 @@ go
    union select 'Breakfast bash', 'Main', 'Butter Muffins', 0
    --union select 'Breakfast bash', 'Appetizer', 'Apple Yogurt Smoothie', 1
    union select 'Super Supper', 'Soup', 'Yemenite Chicken Soup', 1
-   union select 'Super Supper', 'Main', '360-Minute Steak', 1
+   --union select 'Super Supper', 'Main', '360-Minute Steak', 1
    --union select 'Super Supper', 'Main', 'Roasted Vegetable Yapchik', 0
    --union select 'Super Supper', 'Main', 'Eggplant Casserole Dip', 0
    union select 'Super Supper', 'Dessert', 'Chocolate Chip Cookies', 1
