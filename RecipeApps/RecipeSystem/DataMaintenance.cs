027f1ccf5d60157db74d8f69f70aef0e36ab5a41
Ashira Friedman <77624864+AshiraFriedman@users.noreply.github.com>
2023-05-16 10:43:15 +0300

    hw complete

--- RecipeApps/RecipeSystem/DataMaintenance.cs
+++ RecipeApps/RecipeSystem/DataMaintenance.cs
@@ -36,8 +36,6 @@
         {
             if (dt.Rows.Count == 0)
             {
-                //AF What does this error mean and when can this happen?
-                //SM This would throw exception if loaded with wrong table. See my comment in Cookbook class.      
                 throw new Exception("Cannot update date fields, wrong table was sent to system.");
             }
 
